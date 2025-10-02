2766d24062e5a16ea04bd39f34e0e9fd50140615
duynt <duynt@DESKTOP-IJ2V8I3>
2025-10-02 23:27:32 +0700

    First commit - upload Day06Anotation project

---
+++ Day06Anotation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day06Anotation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
